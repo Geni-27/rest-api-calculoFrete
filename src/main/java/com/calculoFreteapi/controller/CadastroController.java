84275959ea309fd1c9d821aa1440c0d90378412b
Genilson <genilson.fa@hotmail.com>
2021-11-04 15:54:37 -0300

    segundo commit

--- src/main/java/com/calculoFreteapi/controller/CadastroController.java
+++ src/main/java/com/calculoFreteapi/controller/CadastroController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,9 +29,9 @@ public class CadastroController {
 		return cadastroRepository.findAll();
 	}
 	
-	@GetMapping("/cadastroFrete/{id}")
+	@GetMapping("/{id}")
 	public CadastroFrete getCadastroFreteById(@PathVariable Long id) {
-		return cadastroRepository.getById(id);
+		return cadastroRepository.findById(id).get();
 	}
 	
 	@PostMapping
@@ -38,5 +40,10 @@ public class CadastroController {
 		return cadastroRepository.save(cadastroFrete);
 	}
 	
-	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.OK)
+	public CadastroFrete deletar(@RequestParam Long id) {
+		cadastroRepository.deleteById(id);
+		return null;
+	}
 }
